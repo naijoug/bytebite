769dc2525778897f40cd14cc0f46fb8563260756
naijoug <naijoug@outlook.com>
2025-11-21 00:01:18 +0800

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GHP_BASE || '/',
   build: {
     // Optimize chunk splitting
     rollupOptions: {
